2b095ca6dfc0e027a507cc1be359fe5ee6e3db6f
EZH1809 <145156906+EZH1809@users.noreply.github.com>
2024-02-18 20:43:13 +0000

    Deploying to gh-pages from @ EZH1809/goit-js-hw-10@7d6160d42a7b800f16d2712eeb01709a25a10a3f 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,2 +1,2 @@
-import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */console.log(2);
+import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */import{i}from"./assets/vendor-77e16229.js";const t=document.querySelector(".form");t.addEventListener("submit",r=>{r.preventDefault();const s=parseInt(t.elements.delay.value,10),m=t.elements.state.value;new Promise((e,o)=>{setTimeout(()=>{m==="fulfilled"?e(s):o(s)},s)}).then(e=>{i.success({title:"Fulfilled promise",message:`✅ Fulfilled promise in ${e}ms`})}).catch(e=>{i.error({title:"Rejected promise",message:`❌ Rejected promise in ${e}ms`})})});
 //# sourceMappingURL=commonHelpers2.js.map
